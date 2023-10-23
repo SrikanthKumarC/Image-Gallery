73fb2c46510441dbc8fc84b00cd8f8b56e402281
SrikanthKumarC <wpsrikanth@gmail.com>
2023-10-24 02:06:43 +0530

    changes v1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,6 @@
 import Search from "./Search";
-import { useEffect, useState } from "react";
 
 function Header({ search, setSearch }) {
-  const [resizeClass, setResizeClass] = useState("");
-
-  useEffect(() => {
-    window.onscroll = function () {
-      scrollFunction();
-    };
-
-    function scrollFunction() {
-      if (
-        document.body.scrollTop > 50 ||
-        document.documentElement.scrollTop > 50
-      ) {
-        setResizeClass("");
-      } else {
-        setResizeClass("lg:py-8 text-2xl lg:text-6xl");
-      }
-    }
-  }, []);
-
   return (
     <header
       className={` transition-all bg-white w-full  flex justify-between lg:px-8  py-2 text-lg lg:text-3xl p-3 gap-2 border`}
